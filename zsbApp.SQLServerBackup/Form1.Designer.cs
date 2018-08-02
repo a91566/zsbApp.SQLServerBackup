3cbab55e141f476a6122b34fe758cbfb8fc07ed5
zhengShaobao <a91566@163.com>
2018-08-02 21:39:21 +0800

    Initial commit

---
+++ zsbApp.SQLServerBackup/Form1.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace zsbApp.SQLServerBackup
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txbServer = new System.Windows.Forms.TextBox();
+            this.txbDatabase = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txbUsername = new System.Windows.Forms.TextBox();
+            this.txbPassword = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.txbServer);
+            this.groupBox1.Controls.Add(this.txbDatabase);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.txbUsername);
+            this.groupBox1.Controls.Add(this.txbPassword);
+            this.groupBox1.Location = new System.Drawing.Point(26, 24);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Size = new System.Drawing.Size(427, 302);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Connection";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.Info;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(88, 225);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(313, 43);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Back-up";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txbServer
+            // 
+            this.txbServer.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txbServer.Location = new System.Drawing.Point(88, 36);
+            this.txbServer.Margin = new System.Windows.Forms.Padding(4);
+            this.txbServer.Name = "txbServer";
+            this.txbServer.Size = new System.Drawing.Size(313, 26);
+            this.txbServer.TabIndex = 0;
+            this.txbServer.Text = ".";
+            // 
+            // txbDatabase
+            // 
+            this.txbDatabase.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txbDatabase.Location = new System.Drawing.Point(88, 83);
+            this.txbDatabase.Margin = new System.Windows.Forms.Padding(4);
+            this.txbDatabase.Name = "txbDatabase";
+            this.txbDatabase.Size = new System.Drawing.Size(313, 26);
+            this.txbDatabase.TabIndex = 1;
+            this.txbDatabase.Text = "zsb";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.ForeColor = System.Drawing.Color.Black;
+            this.label4.Location = new System.Drawing.Point(11, 182);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(72, 16);
+            this.label4.TabIndex = 77;
+            this.label4.Text = "Password";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.Color.Black;
+            this.label3.Location = new System.Drawing.Point(11, 136);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(72, 16);
+            this.label3.TabIndex = 76;
+            this.label3.Text = "UserName";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(11, 89);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 16);
+            this.label2.TabIndex = 75;
+            this.label2.Text = "Database";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(27, 41);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 16);
+            this.label1.TabIndex = 74;
+            this.label1.Text = "Server";
+            // 
+            // txbUsername
+            // 
+            this.txbUsername.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txbUsername.Location = new System.Drawing.Point(88, 130);
+            this.txbUsername.Margin = new System.Windows.Forms.Padding(4);
+            this.txbUsername.Name = "txbUsername";
+            this.txbUsername.Size = new System.Drawing.Size(313, 26);
+            this.txbUsername.TabIndex = 2;
+            this.txbUsername.Text = "sa";
+            // 
+            // txbPassword
+            // 
+            this.txbPassword.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txbPassword.Location = new System.Drawing.Point(88, 177);
+            this.txbPassword.Margin = new System.Windows.Forms.Padding(4);
+            this.txbPassword.Name = "txbPassword";
+            this.txbPassword.PasswordChar = '*';
+            this.txbPassword.Size = new System.Drawing.Size(313, 26);
+            this.txbPassword.TabIndex = 3;
+            this.txbPassword.Text = "sa";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(478, 351);
+            this.Controls.Add(this.groupBox1);
+            this.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.ForeColor = System.Drawing.Color.Black;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "zsbApp.SQLServerBackup";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txbServer;
+        private System.Windows.Forms.TextBox txbDatabase;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txbUsername;
+        private System.Windows.Forms.TextBox txbPassword;
+    }
+}
+
---
+++ zsbApp.SQLServerBackup/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace zsbApp.SQLServerBackup
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string connectionString = this.getConnectionString();
+            if (string.IsNullOrEmpty(connectionString))
+                return;
+            SaveFileDialog dialog = new SaveFileDialog();
+            dialog.FileName = System.DateTime.Now.ToString("yyyyMMdd-HHmmssfff") + ".bak";
+            if (dialog.ShowDialog() != DialogResult.OK)
+                return;
+
+            string fileName = dialog.FileName;
+            //if (File.Exists(fileName))
+            //{
+            //    MessageBox.Show("信息未完整。");
+            //    return;
+            //}
+            this.button1.Enabled = false;
+
+            string sql = string.Format("BACKUP DATABASE [{0}] TO DISK='{1}'", this.txbDatabase.Text.Trim(), fileName);
+
+            System.Timers.Timer t = new System.Timers.Timer(50);
+            t.Enabled = true;
+            t.Elapsed += (a, b) =>
+            {
+                t.Enabled = false;
+                try
+                {
+                    SqlConnection conn = new SqlConnection(connectionString);
+                    conn.Open();
+                    SqlCommand comm = new SqlCommand(sql, conn);
+                    comm.CommandType = CommandType.Text;
+                    comm.ExecuteNonQuery();
+                    conn.Close();
+                    this.setEnabled();
+                    MessageBox.Show("ok");
+                }
+                catch (Exception ex)
+                {
+                    this.setEnabled();
+                    MessageBox.Show(ex.Message);
+                }
+            };
+        }
+
+        public string getConnectionString()
+        {
+            if (string.IsNullOrEmpty(txbServer.Text.Trim()) || string.IsNullOrEmpty(txbDatabase.Text.Trim()) || string.IsNullOrEmpty(txbUsername.Text.Trim()))
+            {
+                MessageBox.Show("信息未完整。");
+                return null;
+            }
+            return string.Format("server={0};database={1};uid={2};pwd={3}", txbServer.Text.Trim(), txbDatabase.Text.Trim(), txbUsername.Text.Trim(), txbPassword.Text);
+        }
+
+        private void setEnabled()
+        {
+            this.Invoke(new Action(() => { this.button1.Enabled = true; }));
+        }
+    }
+}
